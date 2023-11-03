cfbe8bbf512966d85d36dd350ce6b0b4cecbec3a
Defy <61808223+De-y@users.noreply.github.com>
2023-11-02 20:10:17 -0700

    Update projects.vue

--- pages/projects.vue
+++ pages/projects.vue
@@ -20,7 +20,7 @@ import './assets/base.css'
           <h1 class="">Xen (formerly Kosciuszko)</h1>
           <br>
           <p class="card-text">Xen is a new product that we are working on over here @avnce that will be our attempt at shaping the future of hosting.</p>
-          <a class="card-text" href="mailto://hello@avnce.org">Send an email to learn more →</a>
+          <a class="card-text" href="https://github.com/avnce/Xen">Visit GitHub →</a>
         </div>
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#00b7ff" fill-opacity="1" d="M0,32L30,48C60,64,120,96,180,133.3C240,171,300,213,360,234.7C420,256,480,256,540,240C600,224,660,192,720,160C780,128,840,96,900,85.3C960,75,1020,85,1080,117.3C1140,149,1200,203,1260,192C1320,181,1380,107,1410,69.3L1440,32L1440,320L1410,320C1380,320,1320,320,1260,320C1200,320,1140,320,1080,320C1020,320,960,320,900,320C840,320,780,320,720,320C660,320,600,320,540,320C480,320,420,320,360,320C300,320,240,320,180,320C120,320,60,320,30,320L0,320Z" style="--darkreader-inline-fill: #0092cc;" data-darkreader-inline-fill="" class="sv"></path></svg>
       </div>
