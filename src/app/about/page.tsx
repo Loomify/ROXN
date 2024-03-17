8a833d1da946ded1d6763086ef760955d0d682e0
Defy <61808223+De-y@users.noreply.github.com>
2024-03-16 17:31:37 -0700

    history added

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -11,10 +11,15 @@ export default function About() {
             <div className="lead">
                 <div className="mission">
                     <h2>Our Mission</h2>       
-                    <p>Our mission is to be able to provide the software necessary to change the world for the better.</p>        
+                    <p>Our mission is to be able to provide the software necessary to change the world for the better.</p>
                 </div>                
             </div>
-
+            <div className="history">
+                <div>
+                    <h2>Our History</h2>
+                    <p>avnce was founded in July of 2022 (unoficially) by 2 friends as aviance who wanted to build software that would change the world. When one of the friends left, aviance rebranded to avnce and has been working on projects ever since.</p>
+                </div>
+            </div>
         </>
     )
 }
