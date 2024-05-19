940ecd823ec71df238b5bcec18a9a5e0fb213e08
Defy <61808223+De-y@users.noreply.github.com>
2024-05-19 12:44:25 -0700

    issue

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,15 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
 import { Navbar } from "@/components/Navbar";
 import { Footer } from "@/components/Footer";
 
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Loom",
   description: "The community where science and technology intersect to make wonders within applications and research.",
+  openGraph: {
+    images: "/loom_tch_scn.png",
+  },
 };
 
 export default function RootLayout({
@@ -18,7 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body>
         <Navbar />
           {children}
         <Footer />
