338d038be55d57edb891eb504ebaa7a31975e1d1
Defy <61808223+De-y@users.noreply.github.com>
2024-05-19 13:12:53 -0700

    fs

--- src/app/api/announcement/route.ts
+++ src/app/api/announcement/route.ts
@@ -3,7 +3,7 @@ import { NextResponse } from "next/server";
 export function GET(req: Request) {
     return NextResponse.json({
         head: 'avnce is now Loom!',
-        description: 'We have rebranded to Loom! Loom is a new community that builds ontop of the foundation that avnce set. More information will be released soon.',
+        description: 'We have rebranded to Loom! Loom is a new community that builds ontop of the foundation that avnce set. We are excited to see where this new journey takes us!',
         category: 'important',
     })
 }
