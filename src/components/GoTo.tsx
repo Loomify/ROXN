42c87a6b95114f5c079b392d6530b51f930f1db7
Defy <61808223+De-y@users.noreply.github.com>
2024-05-19 10:35:20 -0700

    FIX IT ME

--- src/components/GoTo.tsx
+++ src/components/GoTo.tsx
@@ -2,9 +2,18 @@ import '@/css/components/goto.css'
 import { MdArrowForwardIos } from "react-icons/md";
 
 export function GoTo(params: any) {
+    let href = params.href
+    let className = params.className
+    if (href === undefined) {
+        href = '#'
+    }
+    if (className === undefined) {
+        className = ''
+    }
     return (
         <>
-            <a className={`goto ${params.className}`} href={`${params.href}`}>Go To <span className='arrow'><MdArrowForwardIos /></span></a>
+            {/* @ts-ignore */}
+            <a className={`goto ${className}`} href={`${href}`}>Go To <span className='arrow'><MdArrowForwardIos /></span></a>
         </>
     )
 }
